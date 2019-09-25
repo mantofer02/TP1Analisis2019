16876494b67f015ffa43c0e4e578b5947b9e7bca
Isacc Zúñiga <isaack9734@gmail.com>
2019-09-24 18:27:26 -0600

    se añadio la primera lista, posicionada en LSE

---
+++ listas/lista_pos_LSE.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <sstream>
+using namespace std; 
+
+
+class Posicion {
+	
+private: 	
+Posicion* sucesor; 
+int valor; 
+
+public: 
+Posicion() : sucesor(nullptr) {}
+Posicion(int valor) : valor(valor), sucesor(nullptr) {}
+Posicion(Posicion*siguiente, int valor) : sucesor(siguiente), valor(valor) {}	
+int recuperar() {return this->valor;}
+void modificar(int valor) {this->valor = valor; }
+Posicion* siguiente() {return this->sucesor; }
+void establecerSiguiente(Posicion* siguiente) {this->sucesor = siguiente; }
+	
+};
+
+
+class Lista {
+	
+private: 	
+int numero_elementos; 
+Posicion* primer_elemento; 
+Posicion* ultima_posicion; 
+
+
+public: 
+Lista(); 
+void iniciar(); 											//O.B
+void destruir(); 											//O.B
+void insertar(Posicion* posicion, int valor); 				//O.B
+void agregarAlFinal(int valor); 							//O.B
+void borrar(Posicion * posicion); 							//0.B
+Posicion* primera(); 										//0.B
+//Posicion* siguiente(Posicion* posicion); 
+int NumElem(); 												//O.B
+void agregarAPosicion(int indice, int valor); 				//Algoritmo. 
+void borrarPosicion(int indice); 							//Algoritmo.
+string imprimirLista(); 
+};
+
+
+Lista::Lista() : numero_elementos(0), primer_elemento(nullptr), ultima_posicion(nullptr) {	
+} 
+
+void Lista::iniciar(){	
+	numero_elementos = 0; 
+	primer_elemento = nullptr; 
+	ultima_posicion = nullptr; 
+}
+
+void Lista::destruir() {	
+
+}
+
+int Lista::NumElem() {
+	return this->numero_elementos; 
+}
+
+Posicion* Lista::primera() {
+	return this->primer_elemento; 
+}
+
+
+void Lista::agregarAPosicion(int indice, int valor) {
+	Posicion *temporal = primera(); 
+	if (indice <= NumElem()) {				//si se puede agregar a ese indice. 
+		
+			if (indice != 1) {					//si no quiero agregar al inicio de la lista
+			int indice_actual = 2; 
+			while (indice_actual < indice) {
+				temporal = temporal->siguiente(); 	
+				++indice_actual; 
+			}	
+			insertar(temporal, valor); 			//quiero insertar despues de la posicion temporal. 
+		}
+		else {									//quiero agregar al inicio de la lista.
+		 if (primera() != nullptr) {			//si ya existe algo. 
+			Posicion* nueva_posicion = new Posicion(primera(), valor);
+			this->primer_elemento = nueva_posicion;   
+		 }
+		 else {									//la lista esta vacia. 
+			 Posicion* nueva_posicion = new Posicion(valor);
+			 this->primer_elemento = nueva_posicion;  
+		 }	
+		}	
+			
+		
+	}
+	else {		
+		cout << "voy a agregar al final" << endl; 								//de lo contrario agrega al final. 
+		agregarAlFinal(valor); 
+	}
+}
+
+
+void Lista::insertar(Posicion* posicion, int valor) {
+ if (posicion->siguiente() != nullptr) {
+  Posicion* nueva_posicion = new Posicion(posicion->siguiente(), valor); 
+  posicion->establecerSiguiente(nueva_posicion);
+  ++this->numero_elementos;  	 
+ }
+ else {
+  Posicion* nueva_posicion = new Posicion(valor); 
+  posicion->establecerSiguiente(nueva_posicion); 
+  this->ultima_posicion = nueva_posicion; 
+  ++this->numero_elementos; 
+ }
+	
+}
+
+
+void Lista::agregarAlFinal(int valor) {
+	if (this->numero_elementos != 0) {
+		Posicion* nueva_posicion = new Posicion(valor); 
+		this->ultima_posicion->establecerSiguiente(nueva_posicion); 
+		this->ultima_posicion = nueva_posicion; 
+		++this->numero_elementos; 
+	}
+	else {
+		Posicion* nueva_posicion = new Posicion(valor); 
+		this->primer_elemento = nueva_posicion; 
+		this->ultima_posicion = nueva_posicion; 
+		++this->numero_elementos; 
+	}
+}
+
+
+void Lista::borrar(Posicion* posicion) {							//se entiende que no es el primero. 
+	if (posicion->siguiente() != nullptr) {
+		Posicion * temporal = posicion->siguiente(); 
+		posicion->establecerSiguiente(temporal->siguiente()); 
+		delete temporal; 				
+	}
+	else {
+																	// se quiere eliminar algo que no existe. 	
+	}	
+}
+
+
+
+//void borrarPosicion(int indice); 							//Algoritmo.
+//string imprimirLista(); 
+
+void Lista::borrarPosicion(int indice) {
+	
+	
+}
+
+string Lista::imprimirLista() {
+stringstream ss; 
+
+Posicion* temporal = primera(); 
+
+for (int iteration = 0; iteration < this->numero_elementos; ++iteration) {
+	ss << temporal->recuperar() << " "; 
+	temporal = temporal->siguiente(); 
+} 
+ss << "\n"; 
+
+return ss.str(); 	
+}
+
+
+
+
+int main (int argc, char* argv[]) {
+	
+Lista* lista = new Lista(); 
+lista->iniciar(); 
+
+bool end = true; 
+int answer = -1; 
+int option = -1; 
+int value = 0;
+int position = 0; 
+
+do {
+	
+cout << "ingrese la posicion a ingresar : " << endl; 
+cin>>position; 
+cout << "ingrese el valor :" << endl; 
+cin>>value; 
+
+lista->agregarAPosicion(position, value); 
+cout << lista->imprimirLista() << "\n" << endl; 
+	
+	
+	
+cout << "continuar : 1) SI - 2) NO\n"; 
+cin>>answer; 
+if (answer != 2) {
+	end = false; 
+}	
+else {
+	end = true; 
+}
+	
+}while(!end); 	
+
+
+return 0; 	
+}
