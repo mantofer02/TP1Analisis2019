d324640dd3fd7aa78b0e7eff0dc916b2467c1257
Isacc Zúñiga <isaack9734@gmail.com>
2019-09-28 18:38:44 -0600

    main.cpp

--- listas/lista_pos_Arreglo.h
+++ listas/lista_pos_Arreglo.h
@@ -24,7 +24,7 @@ void agregarPosicion(int posicion, int valor);
 void borrar(int posicion); 
 void borrarPosicion(int posicion); 
 int primera(); 
-int numElem(); 
+int NumElem(); 
 string imprimirLista(); 
 
 
@@ -133,7 +133,7 @@ void Lista::borrar(int posicion) {
  }	
 }
 
-int Lista::numElem() {
+int Lista::NumElem() {
 	return this->numero_elementos; 
 }
 
@@ -142,56 +142,4 @@ void Lista::borrarPosicion(int posicion) {
    posicion = traducir(posicion);
    borrar(posicion);  
 }
-
-
-int main (int argc, char* argv[]) {
-	
-
-	Lista* lista = new Lista(); 
-	lista->iniciar(10); 
-
-	bool end = true; 
-	int answer = -1; 
-	int option = -1; 
-	int value = 0;
-	int position = 0; 
-
-	do {
-		
-	cout << "1) Insertar, 2) Borrar" << endl; 
-	cin >>option; 	
-
-	if (option == 1) {
-		cout << "ingrese la posicion a ingresar : " << endl; 
-		cin>>position; 
-		cout << "ingrese el valor :" << endl; 
-		cin>>value; 
-
-		lista->agregarPosicion(position, value); 
-		cout << lista->imprimirLista() << "\n" << endl; 	
-	}
-	else {
-		cout << "ingrese la posicion a borrar : " << endl; 
-		cin >>position; 
-		lista->borrarPosicion(position); 
-		cout << lista->imprimirLista() << "\n" << endl; 
-	}	
-		
-			
-		
-	cout << "continuar : 1) SI - 2) NO\n"; 
-	cin>>answer; 
-	if (answer != 2) {
-		end = false; 
-	}	
-	else {
-		end = true; 
-	}
-		
-	}while(!end); 	
-		
-	
-}
-
-
 
--- listas/lista_pos_LDE.h
+++ listas/lista_pos_LDE.h
@@ -43,7 +43,7 @@ Posicion* primera();
 Posicion* ultima();		 								//0.B
 //Posicion* siguiente(Posicion* posicion); 
 int NumElem(); 											//O.B
-void AgregarPosicion(int indice, int valor); 			//Algoritmo. //convierte el indice que manda el usuario final a una posicion, para despues insertar
+void agregarPosicion(int indice, int valor); 			//Algoritmo. //convierte el indice que manda el usuario final a una posicion, para despues insertar
 void borrarPosicion(int indice); 						//Algoritmo. //parecido a AgregarPosicion e insertar. 
 string imprimirLista(); 								//Algoritmo. para probar la lista. 
 string imprimirListaRevez(); 
@@ -75,7 +75,7 @@ Posicion* Lista::ultima() {
 	return this->ultima_posicion; 
 }
 
-void Lista::AgregarPosicion(int indice, int valor) {				
+void Lista::agregarPosicion(int indice, int valor) {				
 	Posicion *temporal = primera(); 
 	if (indice <= NumElem()) {						//si se puede agregar a ese indice. 
 		
@@ -208,58 +208,5 @@ for (int iteration = this->numero_elementos; iteration != 0; --iteration) {
 ss << "\n"; 
 
 return ss.str(); 		
-}
-
-
-int main (int argc, char* argv[]) {
-		
-	Lista* lista = new Lista(); 
-	lista->iniciar(); 
-
-	bool end = true; 
-	int answer = -1; 
-	int option = -1; 
-	int value = 0;
-	int position = 0; 
-
-	do {
-		
-	cout << "1) Insertar, 2) Borrar" << endl; 
-	cin >>option; 	
-
-	if (option == 1) {
-		cout << "ingrese la posicion a ingresar : " << endl; 
-		cin>>position; 
-		cout << "ingrese el valor :" << endl; 
-		cin>>value; 
-
-		lista->AgregarPosicion(position, value);
-		cout << "derecho : " << endl;  
-		cout << lista->imprimirLista() << "\n";
-		cout << "revez : " << endl;  
-		cout << lista->imprimirListaRevez() << "\n"; 	
-	}
-	else {
-		cout << "ingrese la posicion a borrar : " << endl; 
-		cin >>position; 
-		lista->borrarPosicion(position); 
-		cout << lista->imprimirLista() << "\n" << endl; 
-	}	
-		
-			
-		
-	cout << "continuar : 1) SI - 2) NO\n"; 
-	cin>>answer; 
-	if (answer != 2) {
-		end = false; 
-	}	
-	else {
-		end = true; 
-	}
-		
-	}while(!end); 	
-
-
-	return 0; 	
 }
 
