1351b6f5af07c94daf308eb4c2a4477f70378534
Isacc Zúñiga <isaack9734@gmail.com>
2019-10-05 19:37:04 -0600

    listas comentadas con clausulas etc

--- listas/lista_pos_Arreglo.cpp
+++ listas/lista_pos_Arreglo.cpp
@@ -5,6 +5,13 @@ Lista_Pos::Lista_Pos() {
 	
 }
 
+
+//Iniciar()
+//Efecto: Este operador básico inicializa la Lista Posicionada, para poder empezar su uso, asignándole memoria. 
+//Requiere:Este operador básico requiere de espacio disponible para inicializar la lista. 
+//Modifica:Este operador básico modifica la memoria, en la cual almacena la lista y los elementos almacenados en las posiciones respectivas. 
+
+
 void Lista_Pos::iniciar() {			//hay que cononcer M.
     this->longitud = length;  
     this->ListaPos = (int*)calloc(longitud, sizeof(int)); 
@@ -12,27 +19,65 @@ void Lista_Pos::iniciar() {			//hay que cononcer M.
 }
 
 
+//Destruir() 
+//Efecto: Este operador básico libera toda la memoria asignada a la lista, tanto los elementos como la lista en sí. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica: Este operador básico modifica la memoria, en la cual liberaría toda la información referente a la lista. 
+
+
 void Lista_Pos::destruir() {
 	this->numero_elementos = 0; 
 }
 
+
+//Vaciar() 
+//Efecto: Este operador básico se encarga de eliminar todos los elementos en todas las posiciones de la Lista Posicionada, sin necesariamente destruir la Lista Posicionada. 
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica:Este operador básico modifica las posiciones de la lista, las cuales van a ser liberadas y borradas de la memoria asignada a la Lista Posicionada. 
+
+
 void Lista_Pos::vaciar() {
 	this->numero_elementos = 0; 
 }
+
+
+//Vacía() 
+//Efecto: Este operador básico retorna un booleano para determinar si la ListaPosicionada no contiene elementos almacenados en ella. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica: Este operador básico no modifica nada. 
+
 
 bool Lista_Pos::vacia() {
 	return (this->numero_elementos == 0); 
 }
 
 
+
+//Primera()
+//Efecto: Este operador básico retorna la primera posición almacenada en la lista.
+//Requiere:Este operador básico requiere de una Lista Posicionada previamente inicializada y una Lista Posicionada no vacía.
+//Modifica: Este operador básico no modifica nada.
+
+
 int Lista_Pos::primera() {
     return 0; 	
 }
+
+//Última()
+//Efecto: Este operador básico retorna la última posición almacenada en la lista.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y una Lista Posicionada no vacía.
+//Modifica: Este operador básico no modifica nada.
 
 
 int Lista_Pos::ultima() {
 	return this->numero_elementos-1; 
 }
+
+//AgregarAlFinal(elemento) 
+//Efecto: Este operador básico agrega un elemento al final de la Lista Posicionada. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica:Este operador básico modifica la Lista, ya que agrega un nuevo último elemento. 
+
 
 void Lista_Pos::agregarAlFinal(int valor) {
 	this->ListaPos[numero_elementos] = valor; 
@@ -45,14 +90,32 @@ int Lista_Pos::traducir(int posicion) {
 	return posicion-1; 	
 } 
 
+//Recupera(posición)
+//Efecto: Este operador básico retorna el valor de un tipo de elemento en una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
+
+
 int Lista_Pos::recuperar(int posicion) {					//requiere que la posicion exista. 		
 	return this->ListaPos[posicion]; 	
 } 
 
 
+//ModificarElemento(elemento, posición) 
+//Efecto: Este operador básico modifica el elemento en una posición determinada, intercambiandolo por un nuevo valor de elemento.
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico modifica una posición determinada, alterando el valor del elemento en esa posición.
+
 void Lista_Pos::modificar(int posicion, int valor) {		//requiere que la posicion exista. 	
 	this->ListaPos[posicion] = valor; 
 }
+
+
+
+//Siguiente(posición)
+//Efecto: Este operador básico retorna la posición siguiente a una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
 
 
 int Lista_Pos::siguiente(int posicion){	
@@ -64,6 +127,12 @@ int Lista_Pos::siguiente(int posicion){
 	 }	
 }
 
+//Anterior(posición)
+//Efecto: Este operador básico retorna la posición anterior a una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
+
+
 int Lista_Pos::anterior(int posicion) {
 	if (posicion < this->numero_elementos && posicion-1 >= 0) {
 		return posicion-1; 
@@ -72,6 +141,12 @@ int Lista_Pos::anterior(int posicion) {
 		return -1; 
 	}
 }
+
+
+//Insertar(elemento, posición) 
+//Efecto: Este operador básico inserta a la Lista Posicionada un determinado elemento enviado por los parámetros a una posición determinada. 
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe ser válida en la Lista Posicionada. 
+//Modifica:Este operador básico modifica las posiciones de la Lista Posicionada insertando un nuevo elemento y posición a la lista. 
 
 
 void Lista_Pos::insertar(int posicion, int valor) {  
@@ -105,6 +180,12 @@ string Lista_Pos::imprimirLista() {
     ss << "\n"; 	
     return ss.str(); 	
 }
+
+
+//Borrar(posición)
+//Efecto: Este operador básico se encarga de borrar un elemento de la Lista Posicionada ubicado en una posición específica.
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico modifica la lista, ya que borra una posición de la misma, donde al elemento posterior a la posición eliminada, se le asignará una nueva posición.
 
 
 void Lista_Pos::borrar(int posicion) {
--- listas/lista_pos_LDE.cpp
+++ listas/lista_pos_LDE.cpp
@@ -3,19 +3,48 @@
 Lista_Pos::Lista_Pos() : numero_elementos(0), primera_posicion(nullptr), ultima_posicion(nullptr) {	
 } 
 
+
+
+//Iniciar()
+//Efecto: Este operador básico inicializa la Lista Posicionada, para poder empezar su uso, asignándole memoria. 
+//Requiere:Este operador básico requiere de espacio disponible para inicializar la lista. 
+//Modifica:Este operador básico modifica la memoria, en la cual almacena la lista y los elementos almacenados en las posiciones respectivas. 
+
 void Lista_Pos::iniciar(){	
 	numero_elementos = 0; 
 	primera_posicion = nullptr; 
 	ultima_posicion = nullptr; 
 }
 
+//Destruir() 
+//Efecto: Este operador básico libera toda la memoria asignada a la lista, tanto los elementos como la lista en sí. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica: Este operador básico modifica la memoria, en la cual liberaría toda la información referente a la lista. 
+
+
 void Lista_Pos::destruir() {	
 	vaciar(); 
 }
 
+
+
+//Vacía() 
+//Efecto: Este operador básico retorna un booleano para determinar si la ListaPosicionada no contiene elementos almacenados en ella. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica: Este operador básico no modifica nada. 
+
+
 bool Lista_Pos::vacia() {
 	return (this->numero_elementos == 0); 
 }
+
+
+
+//Vaciar() 
+//Efecto: Este operador básico se encarga de eliminar todos los elementos en todas las posiciones de la Lista Posicionada, sin necesariamente destruir la Lista Posicionada. 
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica:Este operador básico modifica las posiciones de la lista, las cuales van a ser liberadas y borradas de la memoria asignada a la Lista Posicionada. 
+
 
 void Lista_Pos::vaciar() {
 	Posicion* current_position = primera(); 
@@ -49,30 +78,74 @@ Lista_Pos::Posicion* Lista_Pos::traducir(int indice) {
 	return temp; 
 }
 
+
+//Primera()
+//Efecto: Este operador básico retorna la primera posición almacenada en la lista.
+//Requiere:Este operador básico requiere de una Lista Posicionada previamente inicializada y una Lista Posicionada no vacía.
+//Modifica: Este operador básico no modifica nada.
+
+
 Lista_Pos::Posicion* Lista_Pos::primera() {
 	return this->primera_posicion; 
 }
 
+
+//Siguiente(posición)
+//Efecto: Este operador básico retorna la posición siguiente a una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
+
 Lista_Pos::Posicion* Lista_Pos::siguiente(Posicion* posicion) {
 	return posicion->siguiente(); 
 } 
+
+
+//Anterior(posición)
+//Efecto: Este operador básico retorna la posición anterior a una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
+
 
 Lista_Pos::Posicion* Lista_Pos::anterior(Posicion* posicion) {
 	return posicion->anterior(); 
 }
 
 
+//ModificarElemento(elemento, posición) 
+//Efecto: Este operador básico modifica el elemento en una posición determinada, intercambiandolo por un nuevo valor de elemento.
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico modifica una posición determinada, alterando el valor del elemento en esa posición.
+
 void Lista_Pos::modificar(Posicion* posicion, int valor) {
 	posicion->modificar(valor); 
 }
+
+//Recupera(posición)
+//Efecto: Este operador básico retorna el valor de un tipo de elemento en una posición determinada.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico no modifica nada.
+
 
 int Lista_Pos::recuperar(Posicion* posicion) {
 	return posicion->recuperar(); 
 } 
 
+
+//Última()
+//Efecto: Este operador básico retorna la última posición almacenada en la lista.
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y una Lista Posicionada no vacía.
+//Modifica: Este operador básico no modifica nada.
+
 Lista_Pos::Posicion* Lista_Pos::ultima() {
 	return this->ultima_posicion; 
 }
+
+
+//Insertar(elemento, posición) 
+//Efecto: Este operador básico inserta a la Lista Posicionada un determinado elemento enviado por los parámetros a una posición determinada. 
+//Requiere:Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe ser válida en la Lista Posicionada. 
+//Modifica:Este operador básico modifica las posiciones de la Lista Posicionada insertando un nuevo elemento y posición a la lista. 
+
 
 
 void Lista_Pos::insertar(Posicion* posicion, int valor) {
@@ -120,6 +193,12 @@ void Lista_Pos::insertar(Posicion* posicion, int valor) {
 }
 
 
+//AgregarAlFinal(elemento) 
+//Efecto: Este operador básico agrega un elemento al final de la Lista Posicionada. 
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada. 
+//Modifica:Este operador básico modifica la Lista, ya que agrega un nuevo último elemento. 
+
+
 void Lista_Pos::agregarAlFinal(int valor) {	
 	if (this->numero_elementos != 0) {
 		Posicion* nueva_posicion = new Posicion(ultima(), valor); 
@@ -134,6 +213,12 @@ void Lista_Pos::agregarAlFinal(int valor) {
 		++this->numero_elementos; 
 	}
 }
+
+
+//Borrar(posición)
+//Efecto: Este operador básico se encarga de borrar un elemento de la Lista Posicionada ubicado en una posición específica.
+//Requiere: Este operador básico requiere una Lista Posicionada previamente inicializada y la posición debe pertenecer a la Lista Posicionada. 
+//Modifica: Este operador básico modifica la lista, ya que borra una posición de la misma, donde al elemento posterior a la posición eliminada, se le asignará una nueva posición.
 
 
 void Lista_Pos::borrar(Posicion* posicion) {
