0f8b8dd7e4c6fd51a692d38413791517f72d2d45
Fernando Morales <fezequielmorales@gmail.com>
2019-10-23 13:53:08 -0600

    Primera versión algoritmos ord

---
+++ algoritmos_lista_ord.cpp
@@ -0,0 +1,34 @@
+#include "algoritmos_lista_ord.h"
+
+
+Algoritmos_Ordenada::Algoritmos_Ordenada(Lista_Ord lista){
+    this->lista = lista;
+}
+
+void listar(){
+    this->lista.imprimir();
+}
+
+int iguales(Lista_Ord){
+
+}
+
+void copiar(Lista_Ord){
+    
+}
+
+int contenida(Lista_Ord){
+
+}
+int pertenece(int elemento){
+
+}
+void eliminarElementosRepetidos(Lista_Ord){
+    
+}
+Lista_Ord unionListas(Lista_Ord){
+
+}
+Lista_Ord interseccion(Lista_Ord){
+
+}
---
+++ algoritmos_lista_ord.h
@@ -0,0 +1,20 @@
+#ifndef ALGORITMOS_ORDENADA
+#define ALGORITMOS_ORDENADA
+#include "lista_ord_array.h"
+//#include "lista_ord_LSE.h"
+
+class Algoritmos_Ordenada{
+    private:
+        Lista_Ord lista;
+    public:
+        Algoritmos_Ordenada(Lista_Ord);
+        void listar();
+        int iguales(Lista_Ord);
+        void copiar(Lista_Ord);
+        int contenida(Lista_Ord);
+        int pertenece(int elemento);
+        void eliminarElementosRepetidos(Lista_Ord);
+        Lista_Ord unionListas(Lista_Ord);
+        Lista_Ord interseccion(Lista_Ord);
+}
+#endif
