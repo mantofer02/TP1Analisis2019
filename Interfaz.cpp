1b1a618a33ef0503278a90e2f7f7a67bb6b2003a
Isacc Zúñiga <isaack9734@gmail.com>
2019-10-30 21:28:12 -0600

    bug fixes

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -368,7 +368,12 @@ void Interfaz :: escogioListaPosicionada(){
     std::cout << "10) vaciar();" << std::endl;		    	
     std::cout << "11) destruir();" << std::endl;		
     std::cout << "12) imprimirLista()" << std::endl;
-    std::cout << "13) agregarAlFinal()" << std::endl;				 
+    std::cout << "13) agregarAlFinal()" << std::endl;
+    std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "         Algoritmos         " << std::endl;
+    std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;		 
     std::cout << "15) Burbuja" << std::endl;  
     std::cout << "16) Seleccion" << std::endl; 
     std::cout << "17) Seleccion recursivo" << std::endl;
